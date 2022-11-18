f526c39ef8fae4a8b7f3a01fbdf6540ed149bc77
nickL <nick@nickLs-MacBook-Air.local>
2022-11-17 23:49:35 -0600

    learned how to make scala schema

--- src/main/scala/Tutorial.scala
+++ src/main/scala/Tutorial.scala
@@ -9,5 +9,31 @@ object Tutorial extends App {
 
   Utilities.setupLogging()
 
+  val spark = SparkSession
+    .builder()
+    .appName("YelpReviews")
+    .master("local[*]")
+    .getOrCreate()
 
+//  val firstDataFrame = spark.read
+//    .format("json")
+//    .load("data/InstrumentReviews.json")
+
+
+  val secondDataFrame = spark.read
+    .schema(yelpReviewSchema)
+    .format("json")
+    .load("data/yelp.json")
+
+//  firstDataFrame.show()
+//  firstDataFrame.printSchema()
+
+  secondDataFrame.show()
+  secondDataFrame.printSchema()
+  def getName(): Unit = {
+    secondDataFrame.select("name").show()
+
+  }
+
+ print(getName())
 }
--- src/main/scala/Utilities.scala
+++ src/main/scala/Utilities.scala
@@ -1,6 +1,6 @@
 
 import org.apache.log4j.{Level, Logger}
-import org.apache.spark.sql.types.{ArrayType, DoubleType, IntegerType, LongType, StringType, StructField, StructType}
+import org.apache.spark.sql.types.{ArrayType, DoubleType, IntegerType, LongType, MapType, StringType, StructField, StructType}
 object Utilities {
   def setupLogging() = {
     Logger.getLogger("org.apache.spark").setLevel(Level.ERROR)
@@ -16,4 +16,23 @@ object Utilities {
   /*
    * Write your Yelp Schema below
    */
+
+  val yelpReviewSchema = StructType(Array(
+    StructField("name", StringType, nullable = true),
+    StructField("city", StringType, nullable = true),
+    StructField("stars", DoubleType, nullable = true),
+    StructField("review_count", IntegerType, nullable = true),
+    StructField("state", StringType, nullable = true),
+    StructField("hours", StructType(
+      Array(
+        StructField("Monday", StringType),
+        StructField("Tuesday", StringType),
+        StructField("Wednesday", StringType),
+        StructField("Thursday", StringType),
+        StructField("Friday", StringType),
+        StructField("Saturday", StringType),
+        StructField("Sunday", StringType)
+      )
+    ))
+  ))
 }
